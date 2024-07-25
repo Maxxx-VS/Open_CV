f534b3348e3a4e3adf3b315a131c9f33bcb76b44
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-07-25 21:16:14 +0600

    Распознавание лиц на фото

---
+++ open_cv_faces.py
@@ -0,0 +1,16 @@
+import cv2
+
+img = cv2.imread("images/faces3.jpg")
+img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+faces = cv2.CascadeClassifier('faces.xml')
+
+results = faces.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+for (x, y, w, h) in results:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=3)
+
+cv2.imshow("Result", img)
+cv2.waitKey(0)
